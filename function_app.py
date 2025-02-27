a0a629518c772bbd4fb9651015983556c17a7b66
Me <luca.dellamura@st.oth-regensburg.de>
2025-02-27 16:25:18 +0100

    deleted functions.json/ __init__ as I use the new function_app approach

--- function_app.py
+++ function_app.py
@@ -3,9 +3,10 @@ from scripts.bronze.bronze_processing import ingest_bronze_data
 
 app = func.FunctionApp()
 
+
 @app.function_name(name="BronzeFunction")
 @app.route(route="bronze")
 def main(req: func.HttpRequest) -> func.HttpResponse:
     # Call the bronze layer processing logic
-    result = process_bronze_data(req)
+    result = ingest_bronze_data(req)
     return func.HttpResponse(result, status_code=200)
