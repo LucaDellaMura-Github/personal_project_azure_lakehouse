9b58b6e02ca742948089ab0ec1c70114d99577c6
Me <luca.dellamura@st.oth-regensburg.de>
2025-02-28 21:11:17 +0100

    refractored code snippets into its own functions

--- scripts/bronze/bronze_processing.py
+++ scripts/bronze/bronze_processing.py
@@ -11,11 +11,8 @@ import pyarrow.json as paj
 import io
 from azure.storage.blob import BlobServiceClient
 import datetime
-
-def ingest_bronze_data(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Python HTTP trigger function processed a request.")
-        # Determine the environment and pick the appropriate Key Vault URL
-    app_env = os.getenv("APP_ENV", "test")
+def get_api_key(app_env):
+    # get key vault url depending on environment
     if app_env.lower() == "prod":
         key_vault_url = os.getenv("PROD_KEY_VAULT_URL")
     else:
@@ -27,8 +24,8 @@ def ingest_bronze_data(req: func.HttpRequest) -> func.HttpResponse:
     credential = DefaultAzureCredential()
     client = SecretClient(vault_url= key_vault_url, credential=credential)
     secret = client.get_secret("apikeytmdb")
-    
-    # Fetch movie data
+    return secret
+def fetch_data(secret):
     response_all = []
     for i in range(1, 51):
         url = f"https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page={i}&primary_release_year=2023&sort_by=popularity.desc"
@@ -39,18 +36,41 @@ def ingest_bronze_data(req: func.HttpRequest) -> func.HttpResponse:
        
         response = requests.get(url, headers=headers)
         response_all.append(response.json())  # Append JSON response
+
     
     # Log the total responses fetched
     logging.info(f"Fetched {len(response_all)} pages of movie data.")
+    return response_all
+
+def upload_data_to_azure(app_env,parquet_buffer):
+    # get connection string, container name and blob name to storage depending on environment
+    if app_env.lower() == "prod":
+        connection_string = os.getenv("PROD_Connection_string")
+        container_name = os.getenv("PROD_container_name")
+        blob_name = os.getenv("PROD_blob_name")
+    else:
+        connection_string = os.getenv("TEST_Connection_string")
+        container_name = os.getenv("TEST_container_name")
+        blob_name = os.getenv("TEST_blob_name")
+     # connect to azure storage
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+    # upload to azure storage
+    blob_client.upload_blob(parquet_buffer, overwrite=True)
+
+def ingest_bronze_data(req: func.HttpRequest) -> func.HttpResponse:
+
+    logging.info("Python HTTP trigger function processed a request.")
+        # Determine the environment and pick the appropriate Key Vault URL
+    app_env = os.getenv("APP_ENV", "test")
     
-    # Handle name parameter
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-            name = req_body.get('name')
-        except ValueError:
-            pass
+    secret = get_api_key(app_env, )
+    
+    # Fetch movie data
+    response_all = fetch_data(secret)
+    
+    
     # convert JSON into pyarrow table
     table = paj.read_json(io.BytesIO(json.dumps(response_all).encode()))
 
@@ -72,21 +92,7 @@ def ingest_bronze_data(req: func.HttpRequest) -> func.HttpResponse:
     # Reset buffer position to the beginning
     parquet_buffer.seek(0)
 
-     # get connection string, container name and blob name to storage depending on environment
-    if app_env.lower() == "prod":
-        connection_string = os.getenv("PROD_Connection_string")
-        container_name = os.getenv("PROD_container_name")
-        blob_name = os.getenv("PROD_blob_name")
-    else:
-        connection_string = os.getenv("TEST_Connection_string")
-        container_name = os.getenv("TEST_container_name")
-        blob_name = os.getenv("TEST_blob_name")
-     # connect to azure storage
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-
-    # upload to azure storage
-    blob_client.upload_blob(parquet_buffer, overwrite=True)
+    upload_data_to_azure(app_env,parquet_buffer)
   
 
    
