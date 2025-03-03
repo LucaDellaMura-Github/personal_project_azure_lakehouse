055092f3a627fa015c104edcc5a0752794e530c4
Me <luca.dellamura@st.oth-regensburg.de>
2025-03-03 22:25:22 +0100

    added utils.py

--- scripts/bronze/bronze_processing.py
+++ scripts/bronze/bronze_processing.py
@@ -11,57 +11,8 @@ import pyarrow.json as paj
 import io
 from azure.storage.blob import BlobServiceClient
 import datetime
+from utils import get_api_key, fetch_data, upload_data_to_azure
 
-def get_api_key(app_env):
-    logging.info(f"fetching api key in {app_env}-environment")
-    # get key vault url depending on environment
-    if app_env.lower() == "prod":
-        key_vault_url = os.getenv("PROD_KEY_VAULT_URL")
-    else:
-        key_vault_url = os.getenv("TEST_KEY_VAULT_URL")
-
-    if not key_vault_url:
-        return func.HttpResponse("Key Vault URL not configured.", status_code=500)
-    # Authenticate with Azure Key Vault
-    credential = DefaultAzureCredential()
-    client = SecretClient(vault_url= key_vault_url, credential=credential)
-    secret = client.get_secret("apikeytmdb")
-    return secret
-def fetch_data(secret):
-    logging.info(f"fetching data from api")
-    response_all = []
-    for i in range(1, 51):
-        url = f"https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page={i}&primary_release_year=2023&sort_by=popularity.desc"
-        headers = {
-            "accept": "application/json",
-            "Authorization": f"Bearer {secret.value}"
-        }
-       
-        response = requests.get(url, headers=headers)
-        response_all.append(response.json())  # Append JSON response
-
-    
-    # Log the total responses fetched
-    logging.info(f"Fetched {len(response_all)} pages of movie data.")
-    return response_all
-
-def upload_data_to_azure(app_env,parquet_buffer):
-    logging.info(f"upload data in {app_env}-environment")
-    # get connection string, container name and blob name to storage depending on environment
-    if app_env.lower() == "prod":
-        connection_string = os.getenv("PROD_Connection_string")
-        container_name = os.getenv("PROD_container_name")
-        blob_name = os.getenv("PROD_blob_name")
-    else:
-        connection_string = os.getenv("TEST_Connection_string")
-        container_name = os.getenv("TEST_container_name")
-        blob_name = os.getenv("TEST_blob_name")
-     # connect to azure storage
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-
-    # upload to azure storage
-    blob_client.upload_blob(parquet_buffer, overwrite=True)
 
 def ingest_bronze_data(req: func.HttpRequest) -> func.HttpResponse:
     #entry logging
@@ -82,7 +33,7 @@ def ingest_bronze_data(req: func.HttpRequest) -> func.HttpResponse:
 
     # add data auditability columns
   
-    ingestion_time = pa.array([datetime.now().isoformat()] * len(table))  # Current time for all rows as pyarrow requires
+    ingestion_time = pa.array([datetime.datetime.now().isoformat()] * len(table))  # Current time for all rows as pyarrow requires
     data_source = pa.array(["api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page={i}&primary_release_year=2023&sort_by=popularity.desc"] * len(table))  # Static source name for all rows
 
     # Append metadata columns to the table
---
+++ utils.py
@@ -0,0 +1,64 @@
+import logging
+import azure.functions as func
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+import requests
+import json
+import os
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pyarrow.json as paj
+import io
+from azure.storage.blob import BlobServiceClient
+import datetime
+
+def get_api_key(app_env):
+    logging.info(f"fetching api key in {app_env}-environment")
+    # get key vault url depending on environment
+    if app_env.lower() == "prod":
+        key_vault_url = os.getenv("PROD_KEY_VAULT_URL")
+    else:
+        key_vault_url = os.getenv("TEST_KEY_VAULT_URL")
+
+    if not key_vault_url:
+        return func.HttpResponse("Key Vault URL not configured.", status_code=500)
+    # Authenticate with Azure Key Vault
+    credential = DefaultAzureCredential()
+    client = SecretClient(vault_url= key_vault_url, credential=credential)
+    secret = client.get_secret("apikeytmdb")
+    return secret
+def fetch_data(secret):
+    logging.info(f"fetching data from api")
+    response_all = []
+    for i in range(1, 51):
+        url = f"https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page={i}&primary_release_year=2023&sort_by=popularity.desc"
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {secret.value}"
+        }
+       
+        response = requests.get(url, headers=headers)
+        response_all.append(response.json())  # Append JSON response
+
+    
+    # Log the total responses fetched
+    logging.info(f"Fetched {len(response_all)} pages of movie data.")
+    return response_all
+
+def upload_data_to_azure(app_env,parquet_buffer):
+    logging.info(f"upload data in {app_env}-environment")
+    # get connection string, container name and blob name to storage depending on environment
+    if app_env.lower() == "prod":
+        connection_string = os.getenv("PROD_Connection_string")
+        container_name = os.getenv("PROD_container_name")
+        blob_name = os.getenv("PROD_blob_name")
+    else:
+        connection_string = os.getenv("TEST_Connection_string")
+        container_name = os.getenv("TEST_container_name")
+        blob_name = os.getenv("TEST_blob_name")
+     # connect to azure storage
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+    # upload to azure storage
+    blob_client.upload_blob(parquet_buffer, overwrite=True)
